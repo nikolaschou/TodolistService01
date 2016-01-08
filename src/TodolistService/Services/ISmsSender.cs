daf4d97463b119ca1a16ae10ce28c0d10c97ab88
Nikola <nikolaschou@gmail.com>
2016-01-09 00:41:05 +0100

    Adding web api code

---
+++ src/TodolistService/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodolistService.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
